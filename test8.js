fe2db3311d008e1668f81a720d9153aa38d344da
fathimathzahiya <fathimathuzahiya1@gmail.com>
2026-04-07 12:55:32 +0530

    eight commit

---
+++ test8.js
@@ -0,0 +1,4 @@
+let numbers=[12,23,34,45,56];
+let std={name:"diya",age:21,course:"bca"};
+console.log("Numbers:"+numbers);
+console.log("name:"+std.name +" "+"age:"+std.age+" " +"course:"+std.course);
