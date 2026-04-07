d76d1fee2c264872ee4d86e38c58b162e631d6fb
fathimathzahiya <fathimathuzahiya1@gmail.com>
2026-04-07 12:02:18 +0530

    third commit

---
+++ test3.js
@@ -0,0 +1,9 @@
+let fstname="fathimath";
+let lstname="zahiya";
+let fullname=fstname+" "+lstname;
+let age=20;
+let broname="Rihan";
+console.log("My name is " + fullname);
+console.log("My age is " + age);
+console.log("my brother name is "+broname);
+
