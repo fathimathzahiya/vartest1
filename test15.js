9c01de2e7f88170fa44fb7160a83fb5c2cfd2ad1
fathimathzahiya <fathimathuzahiya1@gmail.com>
2026-04-08 10:29:40 +0530

    15th commit

---
+++ test15.js
@@ -0,0 +1,9 @@
+let score=70;
+if(score>85)
+    console.log("excellent");
+else if(score>60)
+        console.log("good");
+      else if(score>40)
+              console.log("average");
+            else
+                console.log("fail");
